827be6642c7a68b169efc642146376e2a55b161e
daifuku99 <fukushima9952@gmail.com>
2020-07-26 00:06:12 +0900

    addProfiles

--- daifuku_client_dev/src/layouts/MainLayout.vue
+++ daifuku_client_dev/src/layouts/MainLayout.vue
@@ -24,10 +24,7 @@
       content-class="bg-grey-10"
     >
       <q-list>
-        <q-item-label
-          header
-          class="text-white"
-        >
+        <q-item-label header class="text-white">
           Links
         </q-item-label>
         <EssentialLink
@@ -45,45 +42,45 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
+import EssentialLink from "components/EssentialLink.vue";
 
 export default {
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
     EssentialLink
   },
 
-  data () {
+  data() {
     return {
       leftDrawerOpen: false,
       essentialLinks: [
         {
-          title: 'Profiles',
-          caption: 'Who is Daifuku?',
-          icon: 'school',
-          link: 'https://quasar.dev'
+          title: "Profiles",
+          caption: "Who is Daifuku?",
+          icon: "school",
+          link: "/Profiles"
         },
         {
-          title: 'Github',
-          caption: 'github.com/daifuku99/DaifukuProject',
-          icon: 'code',
-          link: 'https://github.com/daifuku99/DaifukuProject'
+          title: "Github",
+          caption: "github.com/daifuku99/DaifukuProject",
+          icon: "code",
+          link: "https://github.com/daifuku99/DaifukuProject"
         },
         {
-          title: 'Twitter',
-          caption: '@99_daiki',
-          icon: 'rss_feed',
-          link: 'https://twitter.com/99_daiki'
+          title: "Twitter",
+          caption: "@99_daiki",
+          icon: "rss_feed",
+          link: "https://twitter.com/99_daiki"
         },
         {
-          title: 'Quasar Awesome',
-          caption: 'Community Quasar projects',
-          icon: 'favorite',
-          link: 'https://awesome.quasar.dev'
+          title: "Quasar Awesome",
+          caption: "Community Quasar projects",
+          icon: "favorite",
+          link: "https://awesome.quasar.dev"
         }
       ]
-    }
+    };
   }
-}
+};
 </script>
--- daifuku_client_dev/src/router/routes.js
+++ daifuku_client_dev/src/router/routes.js
@@ -8,6 +8,14 @@ const routes = [
     ]
   },
 
+  {
+    path: '/Profiles',
+    component: () => import('components/Profiles.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') }
+    ]
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
