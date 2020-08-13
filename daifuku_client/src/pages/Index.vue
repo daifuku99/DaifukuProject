0f76921b0925139066e3cb3643aba2bf9742d474
daifuku99 <fukushima9952@gmail.com>
2020-08-14 00:05:15 +0900

    [DAIFUKU-4]Modifyu Index Pages

--- daifuku_client/src/pages/Index.vue
+++ daifuku_client/src/pages/Index.vue
@@ -1,32 +1,74 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="q-pa-md q-gutter-sm">
-      <q-btn label="Let's Tamatebako" color="black" @click="confirm = true" />
-      <q-dialog v-model="confirm" persistent>
-        <q-card>
-          <q-card-section class="row items-center">
-            <q-avatar color="black" text-color="white" />
-            <span class="q-ml-sm"
-              >Can you permit to connect other web site to Google?</span
-            >
-          </q-card-section>
-
-          <q-card-actions align="right">
-            <q-btn flat label="Cancel" color="primary" v-close-popup />
-            <q-btn
-              flat
-              label="Let's Go"
-              color="primary"
-              v-close-popup,
-              v-on:click="openWindow"
-            />
-          </q-card-actions>
-        </q-card>
-      </q-dialog>
+  <div class="q-pa-md">
+    <div class="q-gutter-md">
+      <q-carousel
+        v-model="slide"
+        transition-prev="scale"
+        transition-next="scale"
+        swipeable
+        animated
+        control-color="white"
+        navigation
+        padding
+        arrows
+        height="300px"
+        class="bg-blue text-white shadow-1 rounded-borders"
+      >
+        <q-carousel-slide name="style" class="column no-wrap flex-center">
+          <q-icon name="account_box" size="100px" />
+          <div class="q-mt-md text-center">
+            {{ Slide1 }}
+            <div class="q-pa-md messageBtn">
+              <q-btn :loading="loading1" color="black" label="Profiles" @click="Profiles"/>
+            </div>
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="tv" class="column no-wrap flex-center">
+          <q-icon name="mood" size="100px" />
+          <div class="q-mt-md text-center">
+            {{ Slide2 }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="layers" class="column no-wrap flex-center">
+          <q-icon name="sms" size="100px" />
+          <div class="q-mt-md text-center">
+            {{ Slide3 }}
+                <div class="q-pa-md q-gutter-sm">
+                  <q-btn label="Let's Tamatebako" color="black" @click="confirm = true" />
+                    <q-dialog v-model="confirm" persistent>
+                      <q-card>
+                        <q-card-section class="row items-center">
+                          <q-avatar color="black" text-color="white" />
+                            <span class="q-ml-sm">Can you permit to connect other web site to Google?</span>
+                        </q-card-section>
+                        <q-card-actions align="right">
+                          <q-btn flat label="Cancel" color="primary" v-close-popup />
+                          <q-btn
+                            flat
+                            label="Let's Go"
+                            color="primary"
+                            v-close-popup,
+                            v-on:click="openWindow"
+                          />
+                        </q-card-actions>
+                      </q-card>
+                    </q-dialog>
+                </div>
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="map" class="column no-wrap flex-center">
+          <q-icon name="terrain" size="100px" />
+          <div class="q-mt-md text-center">
+            {{ Slide4 }}
+            <div class="q-pa-md messageBtn">
+              <q-btn v-on:click="showNotif" color="black" label="Today_Message" />
+            </div>
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
     </div>
-    <div class="q-pa-md messageBtn">
-      <q-btn v-on:click="showNotif" color="black" label="Today_Message" />
-    </div>
+  </div>
   </q-page>
 </template>
 
@@ -50,12 +92,21 @@ export default {
         message: 'nyao',
         color: 'black'
       })
+    },
+    Profiles () {
+      this.$router.push('/Profiles')
     }
   },
   data () {
     return {
       confirm: false,
-      link: 'https://www.google.co.jp/'
+      loading1: false,
+      link: 'https://www.google.co.jp/',
+      slide: 'style',
+      Slide1: 'This Website is Daifuku HomePages. Who is Daifuku?',
+      Slide2: 'Here is My Knowledge :)',
+      Slide3: 'Tamatebako makes you to search other keywords not use in life. Try It!!',
+      Slide4: 'Tap the button, return Today message'
     }
   }
 }
