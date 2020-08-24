c90adc65e955b6189c09941ec5fa2426f366e66e
daifuku99 <fukushima9952@gmail.com>
2020-08-25 00:21:23 +0900

    [DAIFUKU-9]Modify Knowledge Pages

--- daifuku_client/src/pages/Knowledge.vue
+++ daifuku_client/src/pages/Knowledge.vue
@@ -3,7 +3,7 @@
   <div>
     <q-splitter
       v-model="splitterModel"
-      style="height: 500px"
+      style="height: 800px"
     >
 
       <template v-slot:before>
@@ -30,26 +30,80 @@
         >
           <q-tab-panel name="clientside">
             <div class="text-h4 q-mb-md">CLIENT SIDE</div>
-            <div class="text-h5 q-pa-md">◇No.1: paging</div>
-            <div class="q-pa-md knowledge__paging">
-              <q-btn :loading="loading1" label="check" @click="paging" />
-            </div>
-            <div class="text-h5 q-pa-md">◇No.2: framework</div>
-            <div class="q-pa-md knowledge__framework">
-              <q-btn :loading="loading1" label="check" @click="framework" />
-            </div>
+             <div class="q-pa-md row items-start q-gutter-md">
+                <q-card class="knowledge__card">
+                  <img src="https://cdn.quasar.dev/img/mountains.jpg">
+                  <q-card-section>
+                    <div class="text-h6">◇No.1: paging</div>
+                    <div class="text-subtitle2">How do paging</div>
+                  </q-card-section>
+                  <q-card-actions>
+                    <q-btn :loading="loading1" label="check" @click="paging" />
+                  </q-card-actions>
+                </q-card>
+                <q-card class="knowledge__card">
+                  <img src="https://cdn.quasar.dev/img/mountains.jpg">
+                  <q-card-section>
+                    <div class="text-h6">◇No.2: framework</div>
+                    <div class="text-subtitle2">What framework</div>
+                  </q-card-section>
+                  <q-card-actions>
+                    <q-btn :loading="loading1" label="check" @click="framework" />
+                  </q-card-actions>
+                </q-card>
+              </div>
           </q-tab-panel>
 
           <q-tab-panel name="serverside">
             <div class="text-h4 q-mb-md">SERVER SIDE</div>
-            <p>No.1: xxxx</p>
-            <p>No.2: xxxx</p>
+             <div class="q-pa-md row items-start q-gutter-md">
+                <q-card class="knowledge__card">
+                  <img src="https://cdn.quasar.dev/img/mountains.jpg">
+                  <q-card-section>
+                    <div class="text-h6">◇No.1: xxxx</div>
+                    <div class="text-subtitle2">xxxx</div>
+                  </q-card-section>
+                  <q-card-actions>
+                    <q-btn :loading="loading1" label="check" @click="paging" />
+                  </q-card-actions>
+                </q-card>
+                <q-card class="knowledge__card">
+                  <img src="https://cdn.quasar.dev/img/mountains.jpg">
+                  <q-card-section>
+                    <div class="text-h6">◇No.2: xxxx</div>
+                    <div class="text-subtitle2">xxxx</div>
+                  </q-card-section>
+                  <q-card-actions>
+                    <q-btn :loading="loading1" label="check" @click="framework" />
+                  </q-card-actions>
+                </q-card>
+             </div>
           </q-tab-panel>
 
           <q-tab-panel name="tech">
             <div class="text-h4 q-mb-md">TECH</div>
-            <p><b>No.1: xxxx</b></p>
-            <p>No.2: xxxx</p>
+             <div class="q-pa-md row items-start q-gutter-md">
+                <q-card class="knowledge__card">
+                  <img src="https://cdn.quasar.dev/img/mountains.jpg">
+                  <q-card-section>
+                    <div class="text-h6">◇No.1: xxxx</div>
+                    <div class="text-subtitle2">xxxx</div>
+                  </q-card-section>
+                  <q-card-actions>
+                    <q-btn :loading="loading1" label="check" @click="paging" />
+                  </q-card-actions>
+                </q-card>
+                <q-card class="knowledge__card">
+                  <img src="https://cdn.quasar.dev/img/mountains.jpg">
+                  <q-card-section>
+                    <div class="text-h6">◇No.2: xxxx</div>
+                    <div class="text-subtitle2">xxxx</div>
+                  </q-card-section>
+                  <q-card-actions>
+                    <q-btn :loading="loading1" label="check" @click="framework" />
+                  </q-card-actions>
+                </q-card>
+             </div>
           </q-tab-panel>
         </q-tab-panels>
       </template>
